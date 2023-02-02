62451d77e8567463a73b6d2dcc39b85bbf6bef2f
13Dylan13 <dylan.rickerby@capita.com>
2023-02-02 07:25:12 +0000

    Initial tests added

---
+++ test_ticket_validation.py
@@ -0,0 +1,33 @@
+#Ticket 1
+def test_check_that_the_file_loads():
+    #ARRANGE
+    filename='results.csv'
+    expected_rows = 25
+    expected_titles = ['user_id','first_name','last_name','answer_1','answer_2','answer_3']
+    #ACT
+
+    #ASSERT
+
+#Ticket 2
+def test_remove_duplicated_responses():
+    pass
+
+#Ticket 3
+def test_confirm_blank_lines_ignored():
+    pass
+
+#Ticket 4
+def test_confirm_capitalisation_applied():
+    pass
+
+#Ticket 5
+def test_answer_3_validation():
+    pass
+
+#Ticket 6
+def test_output_file_creation():
+    pass
+
+#Ticket 7
+def test_display_clean_results():
+    pass
