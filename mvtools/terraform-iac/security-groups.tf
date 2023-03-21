3c85a67eebbead324353190b659ba857b9d03946
Dylan Rickerby <dylanrickerby@MacBook-Pro.local>
2023-03-21 18:08:07 +0000

    found the missing security groups (but I know I've had these before)

--- mvtools/terraform-iac/security-groups.tf
+++ mvtools/terraform-iac/security-groups.tf
@@ -17,3 +17,39 @@ resource "aws_security_group" "ec2" {
         Name = "multiverse-ec2"
     }
 }
+
+resource "aws_security_group_rule" "alb_from_web_ingress" {
+    type                        = "ingress"
+    from_port                   = 80
+    to_port                     = 80
+    protocol                    = "tcp"
+    cidr_blocks                 = ["0.0.0.0/0"]
+    security_group_id           = aws_security_group.alb.id
+}
+
+resource "aws_security_group_rule" "alb_to_ec2_egress" {
+    type                        = "egress"
+    from_port                   = 80
+    to_port                     = 80
+    protocol                    = "tcp"
+    source_security_group_id    = aws_security_group.ec2.id
+    security_group_id           = aws_security_group.alb.id
+}
+
+resource "aws_security_group_rule" "ec2_from_alb_ingress" {
+    type                        = "ingress"
+    from_port                   = 80
+    to_port                     = 80
+    protocol                    = "tcp"
+    source_security_group_id    = aws_security_group.alb.id
+    security_group_id           = aws_security_group.ec2.id
+}
+
+resource "aws_security_group_rule" "ec2_to_web_egress" {
+    type                        = "egress"
+    from_port                   = 0
+    to_port                     = 65535
+    protocol                    = "tcp"
+    cidr_blocks                 = ["0.0.0.0/0"]
+    security_group_id           = aws_security_group.ec2.id
+}
