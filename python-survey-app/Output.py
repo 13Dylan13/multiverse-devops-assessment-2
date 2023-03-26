cd1a66a20554352b65356f63fabc413ef363aa15
Dylan Rickerby <dylanrickerby@MacBook-Pro.local>
2023-03-26 20:19:40 +0100

    questionnaireestults.py split in two, and added a database

---
+++ python-survey-app/Output.py
@@ -0,0 +1,15 @@
+import sqlite3
+import sys 
+from SqlDB import *
+from app import *
+
+def main():
+    db = sqlite3.connect('answers.db')
+    data = select_all(db, 'answers')
+    output_results(data)
+    data = formatForPrinting(data)
+    db.close()
+
+if __name__ == '__main__':
+    sys.exit(main())
+
